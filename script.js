9a3e3281dbfdbb3b77a5a0c4fc15891b589b39a1
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-04-27 21:37:10 -0300

    styling input-range

---
+++ script.js
@@ -0,0 +1,16 @@
+const slider = document.getElementById("myinput");
+const min = slider.min;
+const max = slider.max;
+const value = slider.value;
+
+slider.style.background = `linear-gradient(to right, hsl(174, 86%, 45%) 0%, hsl(174, 86%, 45%) ${
+  ((value - min) / (max - min)) * 100
+}%, #DEE2E6 ${((value - min) / (max - min)) * 100}%, #DEE2E6 100%)`;
+
+slider.oninput = function () {
+  this.style.background = `linear-gradient(to right, hsl(174, 86%, 45%) 0%, hsl(174, 86%, 45%) ${
+    ((this.value - this.min) / (this.max - this.min)) * 100
+  }%, #DEE2E6 ${
+    ((this.value - this.min) / (this.max - this.min)) * 100
+  }%, #DEE2E6 100%)`;
+};
